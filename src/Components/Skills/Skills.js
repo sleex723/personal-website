d9c5a14d714fb0f46cae92f313c22dd164f2e6e5
sleex89 <samlee723@gmail.com>
2018-05-05 18:00:04 -0700

    added skills

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -10,10 +10,9 @@ const skills = (props) => {
 				</div>
 				<hr />
 				<div className={styles.Skills}>
-					<div>
-						<div className={styles.SkillTitle}>Frontend</div>
+					<div id="col1">
 						<div className={styles.Skill}>
-							<div>Javascript</div>
+							<div className={styles.SkillName}>Javascript</div>
 							<div className={styles.Stars}>
 								<span class="glyphicon glyphicon-star" />
 								<span class="glyphicon glyphicon-star" />
@@ -22,8 +21,319 @@ const skills = (props) => {
 								<span class="glyphicon glyphicon-star" />
 							</div>
 						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>HTML 5</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>CSS 3</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>React</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>React-Redux</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>Node.js</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>Express</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>Axios</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>Bootstrap</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>RESTful API</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>npm</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>git</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>CSS Flexbox</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>CSS Grid</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star-empty" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>Redux-Saga</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star-empty" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>PostgresQL</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star-empty" />
+							</div>
+						</div>
 					</div>
-					<div className={styles.Backend} />
+					<div id="col2">
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>MySQL</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star-empty" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>Webpack</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star-empty" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>jQuery</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star-empty" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>Sass</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star-empty" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>Heroku</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star-empty" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>Scrum</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star-empty" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>Firebase</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star-empty" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>AngularJS</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star-empty" />
+								<span class="glyphicon glyphicon-star-empty" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>Jest</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star-empty" />
+								<span class="glyphicon glyphicon-star-empty" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>Enzyme</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star-empty" />
+								<span class="glyphicon glyphicon-star-empty" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>Mocha</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star-empty" />
+								<span class="glyphicon glyphicon-star-empty" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>Travis CI</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star-empty" />
+								<span class="glyphicon glyphicon-star-empty" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>MongoDB</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star-empty" />
+								<span class="glyphicon glyphicon-star-empty" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>AWS</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star-empty" />
+								<span class="glyphicon glyphicon-star-empty" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>SEO</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star-empty" />
+								<span class="glyphicon glyphicon-star-empty" />
+							</div>
+						</div>
+						<div className={styles.Skill}>
+							<div className={styles.SkillName}>Backbone</div>
+							<div className={styles.Stars}>
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star" />
+								<span class="glyphicon glyphicon-star-empty" />
+								<span class="glyphicon glyphicon-star-empty" />
+								<span class="glyphicon glyphicon-star-empty" />
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>;
